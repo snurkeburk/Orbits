794d14a8c4a9cf5ebf2770fbaa2bef6400fa0d6e
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2021-04-21 02:31:08 +0200

    added pictures of space companies below, started working on other page, started working on scroll animations

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,31 @@ function showSlide(){
         showTools.style.display = "none";
     }
 }
+
+
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
+
+const asd = document.querySelector('.lol');
+const message = document.querySelector('#message');
+
+document.addEventListener('scroll', function () {
+    const messageText = isInViewport(asd) ?
+        'The box is visible in the viewport' :
+        'The box is not visible in the viewport';
+
+    message.textContent = messageText;
+
+}, {
+    passive: true
+});
