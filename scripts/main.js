260ad45d1e6b7045c2a295beecbf66c31f8cd936
Isak2003 <70603096+snurkeburk@users.noreply.github.com>
2021-04-21 15:46:57 +0200

    added js for pressing the company buttons to display text (launch dates), added scroll animation for in and out on buttons, added animation to text, added so arrow hides upon scrolling down

--- scripts/main.js
+++ scripts/main.js
@@ -2,35 +2,112 @@ function showSlide(){
     let showTools = document.getElementById("slideout");
     if (showTools.style.display === "none") {
         showTools.style.display = "flex";
+        console.log(window);
     } else {
         showTools.style.display = "none";
     }
 }
 
 
+// scroll animations
 
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+window.addEventListener("scroll",(event) => {
+    //active screen dimensions
+    let width  = window.innerWidth || document.documentElement.clientWidth || 
+    document.body.clientWidth;
+    let height = window.innerHeight|| document.documentElement.clientHeight|| 
+    document.body.clientHeight;
 
-    );
-}
+    console.log(scrollY)
+    let nasaText = document.getElementById('nasaLaunch');
+    let blueOriginText = document.getElementById('blueOriginLaunch');
+    let spacexText = document.getElementById('spacexLaunch');
 
 
-const asd = document.querySelector('.lol');
-const message = document.querySelector('#message');
+    let nasa = document.getElementById('comp-one')
+    let blueOrigin = document.getElementById('comp-two')
+    let spacex = document.getElementById('comp-three')
+    let arrow = document.getElementById('triangle-down');
+    console.log(height, width);
+    if (scrollY >= height/2 && scrollY <= height*2){
+        nasa.style.animation = "slideInFromLeft 2s 1";
+        nasa.style.visibility="visible";
+        blueOrigin.style.animation = "slideInFromLeft 2s 1";
+        blueOrigin.style.visibility="visible";
+        spacex.style.animation = "slideInFromLeft 2s 1";
+        spacex.style.visibility="visible";
 
-document.addEventListener('scroll', function () {
-    const messageText = isInViewport(asd) ?
-        'The box is visible in the viewport' :
-        'The box is not visible in the viewport';
+        
+        console.log(height, width);
+    }else if (scrollY < height/2 || scrollY > height*2){
+        console.log(height, width);
+        nasa.style.animation = "slideOutFromRight 1s 1";
+        blueOrigin.style.animation = "slideOutFromRight 2s 1";
+        spacex.style.animation = "slideOutFromRight 2s 1";
 
-    message.textContent = messageText;
 
-}, {
-    passive: true
+        // text dissapearing when scrolling down
+        nasaText.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  nasaText.style.display = "none"; }, 1000);
+
+        spacexText.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  spacexText.style.display = "none"; }, 1000);
+
+        blueOriginText.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  blueOriginText.style.display = "none"; }, 1000);
+    }
+
+
+        
+    //hide down arrow
+    if (scrollY >=210){
+        arrow.style.visibility="hidden";
+    } else {
+        arrow.style.visibility="visible";
+    }
+    
 });
+
+
+
+// show launches under buttons 
+
+function displayLaunchNasa() {
+    let nasa = document.getElementById('nasaLaunch');
+   
+    if (nasa.style.display === "none") {
+        nasa.style.animation = "slideInFromTop 1s 1";
+        nasa.style.display = "block";
+        console.log("test");
+    } else {
+        nasa.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  nasa.style.display = "none"; }, 1000);
+    }
+   
+} 
+function displayLaunchBlueOrigin() {
+    let blueOrigin = document.getElementById('blueOriginLaunch');
+   
+    if (blueOrigin.style.display === "none") {
+        blueOrigin.style.animation = "slideInFromTop 1s 1";
+        blueOrigin.style.display = "block";
+        console.log("test");
+    } else {
+        blueOrigin.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  blueOrigin.style.display = "none"; }, 1000);
+    }
+   
+} function displayLaunchSpacex() {
+    let spacex = document.getElementById('spacexLaunch');
+   
+    if (spacex.style.display === "none") {
+        spacex.style.animation = "slideInFromTop 1s 1";
+        spacex.style.display = "block";
+        console.log("test");
+    } else {
+        spacex.style.animation = "slideOutFromBottom 1s 1";
+        setTimeout(() => {  spacex.style.display = "none"; }, 1000);
+        
+    }
+   
+} 
