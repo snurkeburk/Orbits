962e310450ef571426a4e289a52825538740544b
snurkeburk <70603096+snurkeburk@users.noreply.github.com>
2021-04-30 14:57:09 +0200

    added ipad resolution as well as styled for ipad res, "fixed" animations for ipad, added navbar already extended for ipad with hover underline animation

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,7 @@ window.addEventListener("scroll",(event) => {
     let spacex = document.getElementById('comp-three')
     let arrow = document.getElementById('triangle-down');
     console.log(height, width);
+    if (width < 700){
     if (scrollY >= height/2 && scrollY <= height*2){
         nasa.style.animation = "slideInFromLeft 2s 1";
         nasa.style.visibility="visible";
@@ -65,7 +66,30 @@ window.addEventListener("scroll",(event) => {
     } else {
         arrow.style.visibility="visible";
     }
-    
+} else if (width > 700){
+    console.log(scrollY)
+    if (scrollY > 750) {
+         nasa.style.animation = "slideInFromLeft 2s 1";
+        nasa.style.visibility="visible";
+        blueOrigin.style.animation = "slideInFromLeft 3s 1";
+        blueOrigin.style.visibility="visible";
+        spacex.style.animation = "slideInFromLeft 4s 1";
+        spacex.style.visibility="visible";
+        console.log("show");
+
+    } 
+    if (scrollY < 600){
+        console.log("hide");
+        nasa.style.animation = "slideOutFromRight 1s 1";
+        blueOrigin.style.animation = "slideOutFromRight 2s 1";
+        spacex.style.animation = "slideOutFromRight 2s 1";
+        setTimeout(() => {  nasa.style.visibility = "hidden"; }, 1000);
+        setTimeout(() => {  spacex.style.visibility = "hidden"; }, 1000);
+        setTimeout(() => {  blueOrigin.style.visibility = "hidden"; }, 1000);
+
+    }
+
+}
 });
 
 
@@ -89,7 +113,7 @@ function displayLaunchBlueOrigin() {
     let blueOrigin = document.getElementById('blueOriginLaunch');
    
     if (blueOrigin.style.display === "none") {
-        blueOrigin.style.animation = "slideInFromTop 1s 1";
+        blueOrigin.style.animation = "slideInFromTop 1s 1 ";
         blueOrigin.style.display = "block";
         console.log("test");
     } else {
@@ -111,3 +135,32 @@ function displayLaunchBlueOrigin() {
     }
    
 } 
+
+
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    console.log("700");
+  } else {
+    document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+
+//byt ut andra animations mot detta, använd fill-mode forward!!!
+document.getElementById("tunnel").animate([
+  // keyframes
+  { transform: 'translateY(0px)' },
+  { transform: 'translateY(-300px)' }
+], {
+  // timing options
+  duration: 1000,
+  iterations: Infinity
+});
