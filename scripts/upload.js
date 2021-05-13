0f649fbc3d19ddf0c13da2b88b04675d9cdfc484
snurkeburk <70603096+snurkeburk@users.noreply.github.com>
2021-05-13 14:38:26 +0200

    fixed visitor for github pages

--- scripts/upload.js
+++ scripts/upload.js
@@ -4,7 +4,7 @@ let upload = document.getElementById("upload");
 let notification = document.getElementById("developer-notice");
 upload.style.display = "none";
  /*|| linkOfTheWebsiteUserCame == "http://127.0.0.1:5500/index.html"*/ 
-if (linkOfTheWebsiteUserCame == "http://127.0.0.1:5500/visitor.html"){
+if (linkOfTheWebsiteUserCame == "https://snurkeburk.github.io/Orbits/visitor.html"){
     console.log("logged in as visitor")
     nonauthorized = true;
     authorized = false;
